4529007467c5020043d391d9566f31f0de3c71ca
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-06-26 13:57:29 +0200

    Add initial project files

---
+++ XcodePluginsDemo/XcodePluginsDemo/XcodePluginsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodePluginsDemoApp.swift
+//  XcodePluginsDemo
+//
+//  Created by Ahmed M. Hassan on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodePluginsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
